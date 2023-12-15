7dc672ac5a4d4c751623fadfd4440ac8359bfc2c
Son187338 <truongson121220@gmail.com>
2023-12-15 22:09:52 +0700

    feat: son add port socket

--- backend/server.js
+++ backend/server.js
@@ -87,7 +87,7 @@ const PORT = process.env.PORT;
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: 'http://localhost:9000',
+    origin: 'http://localhost:3000',
     methods: ["GET", "POST"]
   },
 });
